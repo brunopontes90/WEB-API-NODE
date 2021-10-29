5cf2b09047d59fccf3a341a0e23a76b1acdbf886
Bruno Pontes <bruno_confessor@hotmail.com>
2021-10-29 16:44:50 -0300

    Criado rota get

--- server.js
+++ server.js
@@ -3,7 +3,3 @@ const app = require('./src/config/custom-express');
 app.listen(3000, () => {
     console.log('Servidor rodando na porta 3000');
 });
-
-app.get('/', (req, res) => {
-    res.send('Rota get');
-});
